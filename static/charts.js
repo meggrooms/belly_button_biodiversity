f8efd723934fa65d178bd9e7e704b276a93dfe9e
Meg Grooms-Gangloff <102067932+meggrooms@users.noreply.github.com>
2022-11-15 15:50:32 -0800

    Add files via upload

--- static/charts.js
+++ static/charts.js
@@ -4,7 +4,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("data/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -34,7 +34,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -104,7 +104,7 @@ function buildMetadata(sample) {
 
 //   // 2. Use d3.json to load and retrieve the samples.json file 
 
-  d3.json("samples.json").then((data) => {
+  d3.json("data/samples.json").then((data) => {
 
 
 
